3b75411acb69eff87ed54fd2b7228c765ec7a47a
TheGreekCuban <theodorealexander92@gmaill.com>
2019-07-31 21:21:41 -0400

    added schema and seeds

---
+++ models/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO clothes (brand_name, item_type, item_color, item_style, price, last_worn, times_worn)
+VALUES
+    ("Calvin Klein", "Pants", "Black", "Casual", 120.22, "2019/06/20", 3),
+    ("Channel", "Accessories", "Gold", "Luxury", 1000.99, "2019/06/22", 10),
+    ("Champion", "Sportswear", "Orange", "Athletic", 29.99, "2018/03/22", 22);
