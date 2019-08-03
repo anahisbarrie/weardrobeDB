cc140072f3c2758da89222974e7522fe7bf8684f
esdalee <es.da.lee@gmail.com>
2019-08-03 14:37:33 -0400

    updated package-lock.json

--- models/example.js
+++ models/example.js
@@ -6,8 +6,7 @@ module.exports = function(sequelize, DataTypes) {
     style: DataTypes.STRING,
     price: DataTypes.STRING,
     lastwore: DataTypes.STRING,
-    description: DataTypes.TEXT,
-  
+    description: DataTypes.TEXT
   });
   return Example;
 };
