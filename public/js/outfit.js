01b794ff1f5d74175e6df09df3aa9bf9908b095c
esdalee <es.da.lee@gmail.com>
2019-08-06 22:39:52 -0400

    randomize outfit

--- public/js/outfit.js
+++ public/js/outfit.js
@@ -24,17 +24,22 @@ var OutfitData = {
 };
 
 
+// for saveOutfit - pass each object into saveOutfit
+
+// Function to get a random outfit
 OutfitData.getOutfits().then(function(data) {
   // console.log("Data!" + JSON.stringify(data));
   // var tops = filter(data, "Top");
   var bottom = filter(data, "Bottom");
+  var top = filter(data, "Top");
+  var accessory = filter(data,"Accessory");
   // var accessories = filter(data, "Accessories");
   // console.info("tops:" + JSON.stringify(tops));
   // console.info("bottoms:" + JSON.stringify(bottom));
   // console.info("accessories:" + JSON.stringify(accessories));
   console.info("random bottom: " + JSON.stringify(getRandom(bottom)));
-  console.info("random bottom: " + JSON.stringify(getRandom(bottom)));
-  console.info("random bottom: " + JSON.stringify(getRandom(bottom)));
+  console.info("random bottom: " + JSON.stringify(getRandom(top)));
+  console.info("random bottom: " + JSON.stringify(getRandom(accessory)));
   console.info("random bottom: " + JSON.stringify(getRandom(bottom)));
   console.info("random bottom: " + JSON.stringify(getRandom(bottom)));
   console.info("random bottom: " + JSON.stringify(getRandom(bottom)));
@@ -54,47 +59,7 @@ var getRandom = function(data) {
   return data[Math.floor(Math.random()*data.length)];
 }
 
-// // Object to generate outfit
-// var dailyOutfit = {
-//     getTop: function(id) {
-//       return $.aja({
-//         type: "GET",
-//         url: "api/top",
-//         data: JSON.stringify(outfit)
-//       });
-//     },
-//     getBottom: function(id) {
-//       return $.ajax({
-//         url: "api/bottom",
-//         type: "GET",
-//         data: JSON.stringify(outfit)
-//       });
-//     },
-//     getAccessories: function(id) {
-//       return $.ajax({
-//         url: "api/accessories",
-//         type: "GET",
-//         data:JSON.stringify(outfit)
-//       });
-//     }
-// };
 
-// function randomItembyType(item_type){
-
-//     return id;
-// }
-
-// // Generate outfit
-// function generateOutfit(){
-
-
-//     randomItembyType("Top")
-
-
-
-// };
-
-
-// // On-click to generate daily outfit
-// $("#generateOutfitBtn").on("click",generatOutfit());
+// On-click to generate daily outfit
+$("#generateOutfitBtn").on("click",generatOutfit());
 
