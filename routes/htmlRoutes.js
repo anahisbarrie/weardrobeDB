1afd85c1c2caebe6162e5d38e15c8e4ca002d4de
TheGreekCuban <theodorealexander92@gmaill.com>
2019-08-07 22:12:25 -0400

    got data to pass through, last step is to input data into chart

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -12,8 +12,12 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -21,16 +25,29 @@ module.exports = function(app) {
   });
 
   //Load statistics page with all the charts and current information for the user
-  app.get("/statistics", function(req, res) {
-    res.render("stats")
+  app.get("/statistics", function (req, res) {
+    db.Clothes.findAll({}).then(function (dbClothes) {
+      console.log(dbClothes.example)
+      res.render("stats", {
+        example: dbClothes
+      })
+      console.log(`DB Clothes: `, dbClothes)
+    })
   })
 
-  app.get("/calendar", function(req, res) {
+  app.get("/api/statistics", function (req, res) {
+    db.Clothes.findAll({}).then(function (dbClothes) {
+      res.json(dbClothes)
+    })
+  })
+
+
+  app.get("/calendar", function (req, res) {
     res.render("calendar")
   })
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
