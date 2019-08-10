925b4515649cea9eaf276862d4b7271f8a08e1dd
rafaella nicoletti <rafaella_np@hotmail.com>
2019-08-10 14:37:30 -0400

    changes

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -6,21 +6,22 @@ var API = {
         });
     }
 };
+
 $('document').ready(function () {
     var items = [];
     API.getExamples().then(function (response) {
         for (var i = 0; i < response.length; i++) {
-            items.push({ title: response[i].type, start: response[i].lastworn })
+            items.push({ title: response[i].type, start: response[i].lastwore , imageLink: response[i].imageLink })
         }
-        console.log(items)
         var calendarEl = document.getElementById("calendar3");
         var calendar = new FullCalendar.Calendar(calendarEl, {
             eventClick: function (info) {
                 // alert('Event: ' + info.event.title);
                 // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
                 // alert('View: ' + info.view.type);
+                console.log(info.event.title)
+                $("exampleModalLabel").text(info.event.title)
                 $('#exampleModal').modal();
-
             },
             plugins: ['interaction', 'dayGrid', 'timeGrid'],
             droppable: true,
@@ -32,8 +33,6 @@ $('document').ready(function () {
                 right: 'dayGridMonth,timeGridWeek,timeGridDay'
             },
             events: items
-
-
 
         });
 
