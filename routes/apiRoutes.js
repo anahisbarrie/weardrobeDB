289e2830bd8bda3a7f80896dc1c34dff82593434
anahisbarrie <anahis.barrie@gmail.com>
2019-08-12 14:23:44 -0400

    cleaning apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,47 +3,47 @@ var uuid4 = require("uuid/v4")
 //multir is for input of data and different files like picture at the same time 
 var multer = require("multer");
 var storage = multer.diskStorage({
-  destination: function(req,file,callback){
-    callback(null,"uploaded_files/")
+  destination: function (req, file, callback) {
+    callback(null, "uploaded_files/")
   },
-  filename: function(req,file,callback){
-    // console.log(file);
+  filename: function (req, file, callback) {
+    console.log(file);
     var originalfilename = file.originalname;
     var filename = uuid4() + "_" + originalfilename
-    callback(null,filename)
+    callback(null, filename)
   }
 
 });
 var upload = multer({
   storage: storage
 });
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
 
-  app.post("/api/examples", upload.single("userpicture"), function(req, res) {
-    // console.log("data from frontend: ", req.body);
-    // console.log("file", req.file);var PORT = process.env.PORT || 3000;
+  app.post("/api/examples", upload.single("userpicture"), function (req, res) {
+    console.log("data from frontend: ", req.body);
+    console.log("file", req.file);
     var PORT = process.env.PORT || 3000;
-  var updatedfilename = "http://localhost:" + PORT + "/uploaded_files/" + req.file.filename
-    var uploadedInfo = { ...req.body, imagelink: updatedfilename}
-    // console.log("updated information with link ", uploadedInfo)
-    db.Example.create(uploadedInfo).then(function(dbExample) {
+    var updatedfilename = "http://localhost:" + PORT + "/uploaded_files/" + req.file.filename
+    var uploadedInfo = { ...req.body, imagelink: updatedfilename }
+    console.log("updated information with link ", uploadedInfo)
+    db.Example.create(uploadedInfo).then(function (dbExample) {
+
       res.json(dbExample);
+      // res.send(req.body)
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 };
-
-
